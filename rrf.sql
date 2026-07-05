4f95b1800f656a2463c56b219565145212857b39
kns <knswork@mail.ru>
2026-07-05 18:35:19 +0300

    fts redo

--- rrf.sql
+++ rrf.sql
@@ -1,5 +1,7 @@
-CREATE OR REPLACE FUNCTION do_query(
-	query	text
+CREATE OR REPLACE FUNCTION compute_rrf_results(
+    run_description TEXT DEFAULT 'rrf',
+    max_results_per_query INTEGER DEFAULT NULL,
+    query_limit INTEGER DEFAULT NULL
 )
 RETURNS INTEGER
 LANGUAGE plpgsql
@@ -9,14 +11,38 @@ DECLARE
     qrec     		record;
     qid	     		INTEGER;
     rrf_k    		INTEGER;
+    full_text_weight	REAL;
     semantic_weight 	REAL;
 BEGIN
     rrf_k = 60;
+    full_text_weight= 0.1;
     semantic_weight=1 - full_text_weight;
 
+    -- 1. Создаём запись эксперимента (run)
+    INSERT INTO run (description) VALUES (run_description)
+    RETURNING id INTO v_run_id;
+
+    if (query_limit is null) then
+	select into query_limit count(*) from query;
+    end if;
+
+    for qid in (select id from query order by id limit query_limit)
+    LOOP
+	select into qrec * from query
+        where id = qid;
+	raise notice 'processing query %', qid;
+        INSERT INTO result (query_id, news_id, run_id, rank)
     with
     full_text as (
-	select news_id,  rank from 
+        select
+	    news.id,
+	    row_number() over(order by ts_rank_cd(fts_article, plainto_tsquery(qrec.query_text)) desc) as rank_ix
+	from
+	    news
+	where
+	    fts_article @@ plainto_tsquery(qrec.query_text)
+	order by rank_ix
+        limit max_results_per_query
     ),
     semantic as (
 	select
@@ -43,7 +69,7 @@ BEGIN
 	coalesce(1.0 / (rrf_k + full_text.rank_ix), 0.0) * full_text_weight +
         coalesce(1.0 / (rrf_k + semantic.rank_ix), 0.0) * semantic_weight
 	desc
-        limit 5;
+        limit max_results_per_query;
     END LOOP;
 
     RETURN v_run_id;
